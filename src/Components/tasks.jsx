b35a49cc2f7e141ea8654c9999a9883c5b9951d1
moktadir-mirul <moktadir.mirul@gmail.com>
2024-09-13 16:35:08 +0600

    basic logic complete

--- src/Components/tasks.jsx
+++ src/Components/tasks.jsx
@@ -1,38 +1,20 @@
 import { Link } from "react-router-dom";
-import { TaskDetails } from "../Pages/TaskDetails";
-import { useEffect, useState } from "react";
 
 
 export const Tasks = (props) => {
-    const {tasks, setTasks, urlGiven} = props;
+    const {tasks, setTasks} = props;
 
-    // const [allTasks, setAllTasks] = useState(tasks)
-    console.log(urlGiven)
-    // const deleteHandler = (taskID) => {
-    //     const updatedArr = tasks.filter((item) => item.id !== taskID);
-    //     setTasks(updatedArr);
-    // }
-    const getAllTasks = () => {
-        fetch(urlGiven)
-        .then((res) => res.json)
-        .then((data) => setTasks(data))
-    }
-    useEffect(() => {
-        getAllTasks();
-    }, [])
-    
     const deleteHandler = (dbs, taskID) => {
         fetch(`http://localhost:3000/${dbs}${taskID}`, 
             {
             method: "DELETE"
         })
         .then(() => {
-            getAllTasks()
+            fetch(`http://localhost:3000/${dbs}`)
+            .then((res) => res.json())
+            .then((data) => setTasks(data))
         })
     }
-
-    console.log(tasks, "tasks");
-    
 
     return(
         <div>
